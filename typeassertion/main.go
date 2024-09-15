44432fd1fdaa43f33ca1b05c71d5672aa75af4a9
Narek Tamazyan <natamazy@gmail.com>
2024-09-15 19:35:59 +0400

    type assertion with deep coping

--- typeassertion/main.go
+++ typeassertion/main.go
@@ -20,6 +20,27 @@ func (cat Cat) getSound() {
 	fmt.Println("meow!")
 }
 
+type hasName interface {
+	printNames()
+}
+
+type dynamicStruct struct {
+	names []string
+}
+
+func (ds *dynamicStruct) printNames() {
+	fmt.Println(ds.names)
+}
+
+func deepCopyDynamicStruct(ds *dynamicStruct) *dynamicStruct {
+	// Create a new slice and copy the data from the original slice
+	newNames := make([]string, len(ds.names))
+	copy(newNames, ds.names)
+
+	// Return a new instance of dynamicStruct with the copied slice
+	return &dynamicStruct{names: newNames}
+}
+
 func main() {
 	someVariables := []interface{}{Dog{}, Cat{}, 15, 6.6}
 
@@ -35,4 +56,22 @@ func main() {
 			fmt.Printf("%T\n", typeToCheck)
 		}
 	}
+
+	fmt.Println()
+
+	// var newDyn hasName = &dynamicStruct{[]string{"Narek"}}
+	// newAssertedDyn := newDyn.(*dynamicStruct)
+	// newAssertedDyn.names[0] = "Alo"
+	// newDyn = newDyn.(*dynamicStruct)
+	// newAssertedDyn.printNames()
+	// newDyn.printNames()
+
+	var newDyn hasName = &dynamicStruct{[]string{"Narek"}}
+
+	newAssertedDyn := deepCopyDynamicStruct(newDyn.(*dynamicStruct))
+
+	newAssertedDyn.names[0] = "Alo"
+
+	newAssertedDyn.printNames()
+	newDyn.printNames()
 }
